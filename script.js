8a9b3e7605ad7973a24ab0ed480fa5fbfbcbc0af
Sobirov S <sobiroff23@gmail.com>
2024-07-22 23:10:03 +0500

    fixed all bags

---
+++ script.js
@@ -0,0 +1,8 @@
+// document.getElementById('open-widget').addEventListener('click', function() {
+//     var supportContent = document.getElementById('support-content');
+//     if (supportContent.classList.contains('hidden')) {
+//         supportContent.classList.remove('hidden');
+//     } else {
+//         supportContent.classList.add('hidden');
+//     }
+// });
